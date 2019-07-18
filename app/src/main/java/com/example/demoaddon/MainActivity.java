bce7b9b95eb76aca90da24f3eddacead5eb48c74
TAN QIAO YUE <17045679@rp.edu.sg>
2019-07-18 16:12:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoaddon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.demoaddon;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
+    }
+}
